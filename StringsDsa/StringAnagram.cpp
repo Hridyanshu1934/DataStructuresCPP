e92b9e4b91db5f920f478ffe4d8ef0272e2ae917
Hridyanshu1934 <hridyanshukhattar@icloud.com>
2024-09-07 18:30:28 +0530

    This is string anagram

---
+++ StringsDsa/StringAnagram.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string AreAnagrams1= "bat";
+    string AreAnagrams2 = "tac";
+
+    int counter = 0;
+
+    for(int i = 0 ; i<AreAnagrams1.length();i++){
+        for(int j = 0;j<AreAnagrams2.length();j++){
+            if(AreAnagrams1[i] == AreAnagrams2[j]){
+                cout<<"The letter - "<<AreAnagrams1[i]<<" is a the position of "<<j<<" in second Anagram"<<endl;
+                counter++;
+            }
+            
+        }
+        
+    }
+    if(counter >= AreAnagrams1.length()){
+
+        cout<<"This is an anagram";
+    }
+
+    else{
+        cout<<"This is not an anagram";
+    }
+}
