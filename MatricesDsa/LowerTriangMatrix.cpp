91038ee4a7b128fc1c73d77651f516ff87935f87
Hridyanshu1934 <hridyanshukhattar@icloud.com>
2024-09-18 23:56:51 +0530

    This is code for checking a symmetric matrix

--- MatricesDsa/LowerTriangMatrix.cpp
+++ MatricesDsa/LowerTriangMatrix.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n = 5;
-    int arr[n][n] ={0};
-    int arrToStoreNonZero [] = {0};
+    int arr[n][n];
 
-    for (int i = 0 ; i<5 ;i++){
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
 
-        for(int j = 0 ; j<5 ;j++){
+    bool UpperTriangularMatrix = true;
+    bool LowerTriangularMatrix = true;
 
-            if(i>=j&&arr[i][j] != 0){
-
-                cout<<i<<" and "<<j<<endl;
-                
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i < j && arr[i][j] != 0) {
+                LowerTriangularMatrix = false;
+            }
+            if (i > j && arr[i][j] != 0) {
+                UpperTriangularMatrix = false;
             }
         }
     }
+    if (UpperTriangularMatrix) {
+        cout << "The matrix is an upper triangular matrix." << endl;
+    } else if (LowerTriangularMatrix) {
+        cout << "The matrix is a lower triangular matrix." << endl;
+    } else {
+        cout << "The matrix is neither upper nor lower triangular." << endl;
+    }
+
+    return 0;
 }
